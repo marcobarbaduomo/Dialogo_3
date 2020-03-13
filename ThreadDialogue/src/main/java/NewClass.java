02e7bde7feec67469c382517b358200e5181a75e
Franco <Franco@PC.station>
2020-03-13 11:53:54 +0100

    Thread2

---
+++ ThreadDialogue/src/main/java/NewClass.java
@@ -0,0 +1,14 @@
+public class NewClass implements Runnable{
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+        
+    public void stampaT1(){
+        System.out.println("Ciao");
+        Thread.sleep(5000);
+    }
+    
+}
---
+++ ThreadDialogue/src/main/java/Thread.java
@@ -0,0 +1,8 @@
+public class Thread{
+    
+    public static void main(String[] args){
+        
+    }
+
+
+}
