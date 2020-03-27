64fad5ac3485f2ef25952a804c07f550829cf510
Franco <Franco@PC.station>
2020-03-27 12:15:23 +0100

    Thread

--- Dialogo_3/src/main/java/InteractFile.java
+++ Dialogo_3/src/main/java/InteractFile.java
@@ -1,7 +1,38 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
 
 public class InteractFile {
     private BufferedReader reader;
-    private String dialogue;
+    private String stringDialogue;
+    private String stringPause;
     
+    InteractFile(){
+        try {
+            reader= new BufferedReader( new FileReader("file.txt"));
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+        }
+    }
+    
+    public String getDialogue(){ //restituisce la string di dialogo 
+        return stringDialogue.substring(4, stringDialogue.length()-1);
+    }
+    
+    public String getActor(){ //restituisce actor1 o actor2 in base al dialogo
+        if(stringDialogue.substring(2).equals("t1")){
+            return "actor1";
+        } else if(stringDialogue.substring(2).equals("t2")){
+            return "actor2";
+        } return "ciaone";
+    }
+    
+    public int getPause(){ // restituisce l'intero della pausa
+        try{
+            return Integer.parseInt(stringPause);
+        }catch(NumberFormatException ex){
+            System.out.println("Pause String not valid");
+            return 0;
+        }
+    }
 }
