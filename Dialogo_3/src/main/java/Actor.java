c9ac1d8928846143679a91909a946f2f2dd1d33d
Franco <Franco@PC.station>
2020-03-27 12:39:52 +0100

    ok

--- Dialogo_3/src/main/java/Actor.java
+++ Dialogo_3/src/main/java/Actor.java
@@ -1,4 +1,14 @@
-public class Actor implements runnable{
+public class Actor implements Runnable{
     private String actorName;
     private InteractFile file;
+    
+    public Actor(String actorName, InteractFile file ){
+        this.actorName= actorName;
+        this.file= file;
+    }
+
+    @Override
+    public void run() {
+        
+    }
 }
--- Dialogo_3/src/main/java/InteractFile.java
+++ Dialogo_3/src/main/java/InteractFile.java
@@ -1,18 +1,43 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class InteractFile {
     private BufferedReader reader;
     private String stringDialogue;
     private String stringPause;
+    private int lines;
     
-    InteractFile(){
+    InteractFile() throws IOException{
         try {
             reader= new BufferedReader( new FileReader("file.txt"));
+            lines= linesCont();
+            reader.close();
+            reader= new BufferedReader( new FileReader("file.txt"));
+            continueReading();
         } catch (FileNotFoundException ex) {
             System.out.println("File not found");
         }
+    }
+    
+    private int linesCont(){
+        String s;
+        int i = 1;
+        try{
+            s= reader.readLine();
+            while(s != null){
+                s = reader.readLine();
+                i++;
+            }
+        }catch(IOException ex){
+            
+        }catch(NullPointerException ex){
+            System.out.println("line cont in null pointer: " +i);
+            return i;
+        }
+        System.out.println("line contout catch: " +i);
+        return i;
     }
     
     public String getDialogue(){ //restituisce la string di dialogo 
@@ -33,6 +58,15 @@ public class InteractFile {
         }catch(NumberFormatException ex){
             System.out.println("Pause String not valid");
             return 0;
+        }
+    }
+    
+    public synchronized void continueReading(){ //vai avanti a leggere
+        try{
+        stringDialogue = reader.readLine();
+        stringPause = reader.readLine();
+        }catch(IOException ex){
+            System.out.println("Error while reading file");
         }
     }
 }
